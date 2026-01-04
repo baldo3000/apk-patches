48fd5296edbc408792b79ced6e866ea30a045b05
Andrea Baldazzi <67961970+baldo3000@users.noreply.github.com>
2026-01-04 19:25:59 +0100

    Update UnlockSubscriptionPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/macrofactor/misc/subscription/UnlockSubscriptionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/macrofactor/misc/subscription/UnlockSubscriptionPatch.kt
@@ -8,7 +8,7 @@ val unlockSubscriptionPatch = bytecodePatch(
     name = "Unlock subscription",
     description = "Unlocks the app.",
 ) {
-    compatibleWith("com.sbs.diet")
+    compatibleWith("com.sbs.train", "com.sbs.diet", "com.sbs.dies", "com.sts.diet")
 
     execute {
         spoofAndroidCertFingerprint.method.removeInstructions(26)
